faed6a2d929faf991595116d7139065353e9cc94
alejo-95 <dmesa283@gmail.com>
2024-04-19 19:09:57 -0500

    Ingreso de nuevas vistas, metodo para salir del app

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ app.config['MYSQL_CURSORCLASS']='DictCursor'
 #mysql=MySQL(app)
 mysql.init_app(app)
 
+# @app.before_request
+# def require_login():
+#     allowed_routes = ['admin', 'registro']
+#     if request.endpoint not in allowed_routes and 'username' not in session:
+#         return redirect(url_for('home'))
 
 @app.route('/')
 def home():
@@ -49,7 +54,12 @@ def login():
         else:
             return render_template('index.html',mensaje='El usuario y/o la contraseña son incorrectos')
         
-    
+@app.route('/logout')
+def logout():
+    #Remover el nombre de usuario de la sesión si está presente
+    #session.pop('username', None)
+    session.clear()
+    return redirect(url_for('home'))
 
 @app.route('/registro')
 def registro():
@@ -77,11 +87,17 @@ def createRegister():
 def ListUser():
     cur=mysql.connection.cursor()
 
-    cur.execute('SELECT * FROM usuarios')
+    # cur.execute('SELECT * FROM usuarios')
+    # data2 = cur.fetchall()
+    
+    cur.execute('SELECT u.id, u.name, u.password, p.perfiles FROM usuarios u inner join perfiles p on u.idperfil = p.idperfiles')
     data = cur.fetchall()
-
+    
+    cur.execute('SELECT idperfiles, perfiles FROM perfiles ')
+    dataOpt = cur.fetchall()
+    
     cur.close()
-    return render_template('Users/list_user.html', users = data)
+    return render_template('Users/list_user.html', users = data, dataOpt= dataOpt)
 #editar
 @app.route('/edit/<id>', methods = ['POST', 'GET'])
 def get_user(id):
@@ -131,26 +147,27 @@ def user():
 #agregar
 @app.route('/add-user', methods=["POST","GET"])
 def addUser():
+    cur = mysql.connection.cursor()
     correo = request.form['txtCorreo']
     password = request.form['txtPassword']
-    
-    if correo == '':
-        return #render_template('Users/create_user.html', registerWrong='Por favor ingrese un usuario valido')
-    elif password == '':
-        return #render_template('Users/create_user.html', registerWrong='Por favor ingrese un usuario valido')
-    # if not correo or not password:
-    #     return render_template('Users/create_user.html', registerWrong='Por favor ingrese un usuario válido')
+    perfil_id = request.form['perfil']
+        
+    if correo == '' or password == '' or perfil_id =='':
+        flash('Por favor inrgese un usuario valido','danger')
+        return redirect(url_for('ListUser'))
     else:
 
         try:
-            cur = mysql.connection.cursor()
-            cur.execute("INSERT INTO usuarios (name, password, idperfil) VALUES (%s, %s, '2')", (correo, password))
+            
+            cur.execute("INSERT INTO usuarios (name, password, idperfil) VALUES (%s, %s, %s)", (correo, password, perfil_id))
             mysql.connection.commit()
-            flash('Usuario agregado con exito')
+            cur.close()
+            flash('Usuario agregado con exito', 'success')
             return redirect(url_for('ListUser'))
         except Exception as e:
 
             return #render_template('Users/create_user.html', registerWrong='Error al registrar el usuario')
+
 
 #Metodos para perfil
 #listar
@@ -200,17 +217,16 @@ def addPerfil():
     perfil = request.form['txtPerfil']
     state = request.form['txtEstado']
     
-    if perfil == '':
-        return 
-    elif state == '':
-        return 
+    if perfil == '' or state == '':
+        flash('Por favor inrgese un perfil valido', 'danger')
+        return redirect(url_for('ListPerfil'))
     else:
 
         try:
             cur = mysql.connection.cursor()
             cur.execute("INSERT INTO perfiles (perfiles, estado) VALUES (%s, %s)", (perfil, state))
             mysql.connection.commit()
-            flash('Perfil agregado con exito')
+            flash('Perfil agregado con exito', 'success')
             return redirect(url_for('ListPerfil'))
         except Exception as e:
 
@@ -394,21 +410,263 @@ def addTipClient():
     tip = request.form['txtTip']
     state = request.form['txtEstado']
     
-    if tip == '':
-        return 
-    elif state == '':
-        return 
+    if tip == '' or state == '':
+        flash('Por favor ingrese un tipo de cliente valido', 'danger')
+        return redirect(url_for('ListTipClient')) 
     else:
 
         try:
             cur = mysql.connection.cursor()
             cur.execute("INSERT INTO tipo_clientes (tipo, estado) VALUES (%s, %s)", (tip, state))
             mysql.connection.commit()
-            flash('Tipo cliente agregado con exito')
+            flash('Tipo cliente agregado con exito', 'success')
             return redirect(url_for('ListTipClient'))
         except Exception as e:
 
             return 
+
+
+#Metodos para tamaños de invernadero
+#Listar
+@app.route('/listar-tamanio')
+def ListTamanio():
+    cur=mysql.connection.cursor()
+
+    # cur.execute('SELECT * FROM tamanios_invernadero')
+    cur.execute('SELECT idtamanios, tamanio ,CASE WHEN estado = 0 THEN \'Habilitado\' WHEN estado = 1 THEN \'Deshabilitado\' END AS status FROM tamanios_invernadero')
+    data = cur.fetchall()
+
+    cur.close()
+    return render_template('invernaderos/list_tamanio_inv.html', tam = data)
+#Actualizar
+@app.route('/updateTamanio/<id>', methods=['POST'])
+def updateTamanio(id):
+    if request.method == 'POST':
+        tam = request.form['txtTam']
+        state = request.form['txtEstado']
+        
+        if tam == '' or state == "":
+            flash('Por favor ingrese un tamaño valido', 'danger')
+            return redirect(url_for('ListTamanio'))
+        else:
+            cur=mysql.connection.cursor()
+            cur.execute("""
+                UPDATE tamanios_invernadero
+                SET tamanio = %s,
+                    estado = %s
+                WHERE idtamanios= %s
+            """, (tam, state,  id))
+        mysql.connection.commit()
+        cur.close()
+        flash('Tamaño actualizado exitosamente', 'success')
+        return redirect(url_for('ListTamanio'))
+#Eliminar
+@app.route('/deleteTamanio/<string:id>', methods = ['POST','GET'])
+def deleteTamanio(id): 
+    cur=mysql.connection.cursor()
+    
+    cur.execute('DELETE FROM tamanios_invernadero WHERE idtamanios = {0}'.format(id))
+    mysql.connection.commit()
+    cur.close()
+    flash('Tipo cliente eliminado exitosamente', 'success')
+    return redirect(url_for('ListTamanio'))
+#Agregar
+@app.route('/add-tam', methods=["POST","GET"])
+def addTamanio():
+    tam = request.form['txtTam']
+    state = request.form['txtEstado']
+    
+    if tam == '' or state == '':
+        flash('Por favor ingrese un tamaño valido', 'danger')
+        return redirect(url_for('ListTamanio')) 
+    else:
+
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO tamanios_invernadero (tamanio, estado) VALUES (%s, %s)", (tam, state))
+            mysql.connection.commit()
+            cur.close()
+            flash('Tamaño de invernadero agregado con exito', 'success')
+            return redirect(url_for('ListTamanio'))
+        except Exception as e:
+
+            return 
+
+
+#Metodos para cultivos
+#Listar
+@app.route('/listar-cultivos')
+def ListCultivo():
+    cur=mysql.connection.cursor()
+
+    # cur.execute('SELECT * FROM cultivos')
+    cur.execute('SELECT idcultivos, cultivo, temp_min, temp_max ,CASE WHEN estado = 0 THEN \'Habilitado\' WHEN estado = 1 THEN \'Deshabilitado\' END AS status FROM cultivos')
+    data = cur.fetchall()
+
+    cur.close()
+    return render_template('invernaderos/list_cultivos.html', temp = data)
+#Actualizar
+@app.route('/updateCultivo/<id>', methods=['POST'])
+def updateCultivo(id):
+    if request.method == 'POST':
+        cul = request.form['txtCulti']
+        temMin= request.form['txtTempMin']
+        temMax= request.form['txtTempMax']
+        #state = request.form['txtEstado']
+        
+        if cul == ''  or temMin == '' or temMax == "":
+            flash('Por favor ingrese un cultivo valido', 'danger')
+            return redirect(url_for('ListCultivo'))
+        else:
+            cur=mysql.connection.cursor()
+            cur.execute("""
+                UPDATE cultivos
+                SET cultivo = %s,
+                    temp_min = %s,
+                    temp_max = %s
+                WHERE idcultivos= %s
+            """, (cul, temMin, temMax,  id))
+        mysql.connection.commit()
+        cur.close()
+        flash('Cultivo actualizado exitosamente', 'success')
+        return redirect(url_for('ListCultivo'))
+#Eliminar
+@app.route('/deleteCultivo/<string:id>', methods = ['POST','GET'])
+def deleteCultivo(id): 
+    cur=mysql.connection.cursor()
+    
+    cur.execute('DELETE FROM cultivos WHERE idcultivos = {0}'.format(id))
+    mysql.connection.commit()
+    cur.close()
+    flash('Cultivo eliminado exitosamente', 'success')
+    return redirect(url_for('ListCultivo'))
+#Agregar
+@app.route('/add-cult', methods=["POST","GET"])
+def addCultivo():
+    cul = request.form['txtCulti']
+    temMin= request.form['txtTempMin']
+    temMax= request.form['txtTempMax']
+    state = request.form['txtEstado']
+    
+    if cul == '' or state == "" or temMin == '' or temMax == "":
+        flash('Por favor ingrese un cultivo valido', 'danger')
+        return redirect(url_for('ListCultivo'))
+    else:
+
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO cultivos (cultivo, temp_min, temp_max, estado) VALUES (%s,%s,%s,%s)", (cul, temMin, temMax, state))
+            mysql.connection.commit()
+            cur.close()
+            flash('Cultivo agregado con exito', 'success')
+            return redirect(url_for('ListCultivo'))
+        except Exception as e:
+
+            return 
+
+
+#Metodos para clientes
+#listar
+@app.route('/listar-clientes')
+def ListClient():
+    cur=mysql.connection.cursor()
+
+    # cur.execute('SELECT * FROM usuarios')
+    # data2 = cur.fetchall()
+    
+    cur.execute(""" select 
+                c.idclientes, c.nombre_1, c.nombre_2, c.apellido_1, c.apellido_2, 
+                c.cedula, c.telefono, c.correo, tc.tipo, Case when c.estado = 0 then 'Habilitado' When c.estado = 1 then 'Deshabilitado' 
+                End as status
+                from clientes c
+                inner join tipo_clientes tc on c.tipo_cliente = tc.idtipo_clientes""")
+    data = cur.fetchall()
+    
+    cur.execute('SELECT idtipo_clientes, tipo FROM tipo_clientes ')
+    dataOpt = cur.fetchall()
+    
+    cur.close()
+    return render_template('tipo_cliente/clientes.html', client = data, dataOpt= dataOpt)
+#Actualizar
+@app.route('/updateCliente/<id>', methods=['POST'])
+def updateCliente(id):
+    print(request.form)
+    if request.method == 'POST':
+        nam1 = request.form['txtName1']
+        nam2= request.form['txtName2']
+        ape1= request.form['txtAp1']
+        ape2 = request.form['txtAp2']
+        cc = request.form['txtCc']
+        tel = request.form['txtTel']
+        mail = request.form['txtMail']
+        tipe = request.form['tipoPersona']
+        state = request.form['state']
+        
+        if nam1 == '' or ape1 == "" or cc == '' or tel == '' or mail =='' or tipe =='':
+            flash('Por favor ingrese un cliente valido', 'danger')
+            return redirect(url_for('ListClient'))
+        else:
+            cur=mysql.connection.cursor()
+            cur.execute("""
+                UPDATE clientes
+                SET nombre_1 = %s,
+                    nombre_2 = %s,
+                    apellido_1 = %s,
+                    apellido_2 = %s,
+                    cedula = %s,
+                    telefono = %s,
+                    correo = %s,
+                    tipo_cliente = %s,
+                    estado = %s
+                WHERE idclientes= %s
+            """, (nam1,nam2,ape1,ape2,cc,tel,mail,tipe,state,  id))
+        mysql.connection.commit()
+        cur.close()
+        flash('Cliente actualizado exitosamente', 'success')
+        return redirect(url_for('ListClient'))
+#Elimnar
+@app.route('/deleteCliente/<string:id>', methods = ['POST','GET'])
+def deleteCliente(id): 
+    cur=mysql.connection.cursor()
+    
+    cur.execute('DELETE FROM clientes WHERE idclientes = {0}'.format(id))
+    mysql.connection.commit()
+    cur.close()
+    flash('Cliente eliminado exitosamente', 'success')
+    return redirect(url_for('ListClient'))
+#Agregar
+@app.route('/add-clientes', methods=["POST","GET"])
+def addClient():
+    #print(request.form)
+    nam1 = request.form['txtName1']
+    nam2= request.form['txtName2']
+    ape1= request.form['txtAp1']
+    ape2 = request.form['txtAp2']
+    cc = request.form['txtCc']
+    tel = request.form['txtTel']
+    mail = request.form['txtMail']
+    tipe = request.form['tipoPersona']
+    state = request.form['state']
+    #print(nam1)
+    if nam1 == '' or ape1 == "" or cc == '' or tel == '' or mail =='' or tipe =='':
+        flash('Por favor ingrese un cliente valido', 'danger')
+        return redirect(url_for('ListClient'))
+    else:
+
+        try:
+            cur = mysql.connection.cursor()
+            #cur.execute("INSERT INTO clientes (nombre_1, nombre_2,)values(%s,%s)",(nam1, nam2))
+            cur.execute("INSERT INTO clientes (nombre_1, nombre_2, apellido_1, apellido_2, cedula, telefono, correo, tipo_cliente, estado) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)", (nam1,nam2,ape1,ape2,cc,tel,mail,tipe,state))
+            mysql.connection.commit()
+            cur.close()
+            flash('Cliente agregado con exito', 'success')
+            return redirect(url_for('ListClient'))
+        except Exception as e:
+            flash('Error al agregar el cliente: {}'.format(str(e)), 'danger')
+            return redirect(url_for('ListClient'))
+
+
+
 
 
 
